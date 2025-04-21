8d6a0a18bba85c5c043f09277ebcb99939fad997
Sayuri Witharana <sayuriwitharana@gmail.com>
2025-04-21 19:46:27 +0530

    Updated services page

--- salon_services/views.py
+++ salon_services/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
-def services_page(request):
-    return render(request, 'salon_services/services.html')
+#def services_page(request):
+#    return render(request, 'salon_services/services.html')
 
+
+def services_page(request):
+    service_data = {
+        "Hair": [
+            {"name": "Haircut", "price": 1200},
+            {"name": "Hair Coloring", "price": 2500},
+        ],
+        "Face": [
+            {"name": "Facial Treatment", "price": 2000},
+            {"name": "Makeup", "price": 3000},
+        ],
+        "Nails": [
+            {"name": "Manicure", "price": 1500},
+            {"name": "Pedicure", "price": 1800},
+        ],
+    }
+    return render(request, 'salon_services/services.html', {"service_data": service_data})
