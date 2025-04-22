e3ea75221a68f998359c5ffe356293aae7600c42
ROSEHNI02 <mishelkulathunga@gmail.com>
2025-04-22 14:15:33 +0530

    Updated the branch

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,16 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views. Home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-import views
+from booking import views  # ✅ Make sure to import views from booking
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls', namespace='booking')),
     path('', include('accounts.urls')),
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # ✅ This should be from booking.views
 ]
+
+# ✅ This enables serving static files like CSS and images in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
