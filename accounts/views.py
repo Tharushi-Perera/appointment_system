5316d96da44e3c44d6babdea443acf2ba2855ae0
Sanjhana2710 <sanjhanaw@gmail.com>
2025-05-03 22:35:29 +0530

    Ask user to log in after signing up

--- accounts/views.py
+++ accounts/views.py
@@ -8,12 +8,12 @@ def register_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # Redirect to home after successful signup
+            form.save()
+            return redirect('login')  # Ask user to log in after signing up
     else:
         form = SignUpForm()
     return render(request, 'accounts/register.html', {'form': form})
+
 
 def home(request):
     return render(request, 'home.html')  # Correct path, Django looks inside templates/
