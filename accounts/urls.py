37bfd77c65ced503cd4ccd4b32cea9a9f4ce63a1
Tharushi Perera <tharushiperera991@gmail.com>
2025-05-05 22:29:31 +0530

    fixed auth URL routing and template references

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from accounts import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),  #
     path('logout/', views.logout_view, name='logout'),
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name='accounts/password_reset.html'
     ), name='password_reset'),
