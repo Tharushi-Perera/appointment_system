3f255e85278d7b274d5e77fb361559e810087a47
Sanjhana2710 <sanjhanaw@gmail.com>
2025-05-06 10:14:17 +0530

    logout view

--- accounts/views.py
+++ accounts/views.py
@@ -49,7 +49,7 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     messages.info(request, "Logged out.")
-    return redirect('login')
+    return redirect('accounts:login')
 
 
 @login_required
