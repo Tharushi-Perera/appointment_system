e6c514a21aad120bf090f70f59fd296beb6270b1
Sanjhana2710 <sanjhanaw@gmail.com>
2025-05-06 12:23:40 +0530

    delete account

--- accounts/urls.py
+++ accounts/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='accounts/password_reset_complete.html'
     ), name='password_reset_complete'),
+    path('delete/', views.delete_account, name='delete_account'),
 ]
